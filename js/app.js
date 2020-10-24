664ab82ea8135068594c6643e99a48cd061dc12d
Noxde <70549780+Noxde@users.noreply.github.com>
2020-10-24 09:01:42 -0300

    save

--- js/app.js
+++ js/app.js
@@ -6,28 +6,28 @@ let Password = document.getElementById("pass");
 let ErrorI = document.getElementById("err-icon");
 
 function TryClaim(){
-    if (FirstName.value == "") {
-       FirstName.classList.add("invalid");
-       FirstName.previousElementSibling.style.display = "block";
-        return false;
-    } else {
-        FirstName.previousElementSibling.style.display = "none";
-        FirstName.classList.remove("invalid");
-    }
-    if (LastName.value == "") {
-        LastName.classList.add("invalid");
-        LastName.previousElementSibling.style.display = "block";
-        return false;
-    } else {
-        LastName.previousElementSibling.style.display = "none";
-        LastName.classList.remove("invalid");
-    }
-    if (Email.value == "") {
-       Email.classList.add("invalid");
-       Email.previousElementSibling.style.display = "block";
-         return false;
-     } else {
-        Email.previousElementSibling.style.display = "none";
-        Email.classList.remove("invalid");
-     }
+    // if (FirstName.value == "") {
+    //    FirstName.classList.add("invalid");
+    //    FirstName.previousElementSibling.style.display = "block";
+    //     return false;
+    // } else {
+    //     FirstName.previousElementSibling.style.display = "none";
+    //     FirstName.classList.remove("invalid");
+    // }
+    // if (LastName.value == "") {
+    //     LastName.classList.add("invalid");
+    //     LastName.previousElementSibling.style.display = "block";
+    //     return false;
+    // } else {
+    //     LastName.previousElementSibling.style.display = "none";
+    //     LastName.classList.remove("invalid");
+    // }
+    // if (Email.value == "") {
+    //    Email.classList.add("invalid");
+    //    Email.previousElementSibling.style.display = "block";
+    //      return false;
+    //  } else {
+    //     Email.previousElementSibling.style.display = "none";
+    //     Email.classList.remove("invalid");
+    //  }
 }
