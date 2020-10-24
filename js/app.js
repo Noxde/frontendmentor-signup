aeba9572794eee238dfdae0d0e5d8ce06ac5f767
Noxde <70549780+Noxde@users.noreply.github.com>
2020-10-24 04:11:19 -0300

    figuring out js

---
+++ js/app.js
@@ -0,0 +1,33 @@
+// inputs
+let FirstName = document.getElementById("first-name");
+let LastName = document.getElementById("last-name");
+let Email = document.getElementById("email");
+let Password = document.getElementById("pass");
+let ErrorI = document.getElementById("err-icon");
+
+function TryClaim(){
+    if (FirstName.value == "") {
+       FirstName.classList.add("invalid");
+       FirstName.previousElementSibling.style.display = "block";
+        return false;
+    } else {
+        FirstName.previousElementSibling.style.display = "none";
+        FirstName.classList.remove("invalid");
+    }
+    if (LastName.value == "") {
+        LastName.classList.add("invalid");
+        LastName.previousElementSibling.style.display = "block";
+        return false;
+    } else {
+        LastName.previousElementSibling.style.display = "none";
+        LastName.classList.remove("invalid");
+    }
+    if (Email.value == "") {
+       Email.classList.add("invalid");
+       Email.previousElementSibling.style.display = "block";
+         return false;
+     } else {
+        Email.previousElementSibling.style.display = "none";
+        Email.classList.remove("invalid");
+     }
+}
