c4c3ab3beb94edacc9673b7484db7ae3d3749ce9
Noxde <70549780+Noxde@users.noreply.github.com>
2020-10-24 13:01:35 -0300

    still struggling with js

--- js/app.js
+++ js/app.js
@@ -1,33 +1,54 @@
-// inputs
-let FirstName = document.getElementById("first-name");
-let LastName = document.getElementById("last-name");
-let Email = document.getElementById("email");
-let Password = document.getElementById("pass");
-let ErrorI = document.getElementById("err-icon");
+// let FirstName = document.getElementById("first-name");
+// let LastName = document.getElementById("last-name");
+// let Email = document.getElementById("email");
+// let Password = document.getElementById("pass");
+// let ErrorI = document.getElementById("err-icon");
 
-function TryClaim(){
-    // if (FirstName.value == "") {
-    //    FirstName.classList.add("invalid");
-    //    FirstName.previousElementSibling.style.display = "block";
-    //     return false;
-    // } else {
-    //     FirstName.previousElementSibling.style.display = "none";
-    //     FirstName.classList.remove("invalid");
-    // }
-    // if (LastName.value == "") {
-    //     LastName.classList.add("invalid");
-    //     LastName.previousElementSibling.style.display = "block";
-    //     return false;
-    // } else {
-    //     LastName.previousElementSibling.style.display = "none";
-    //     LastName.classList.remove("invalid");
-    // }
-    // if (Email.value == "") {
-    //    Email.classList.add("invalid");
-    //    Email.previousElementSibling.style.display = "block";
-    //      return false;
-    //  } else {
-    //     Email.previousElementSibling.style.display = "none";
-    //     Email.classList.remove("invalid");
-    //  }
+const regForm = document.querySelector('form#register')
+const errors_el = document.querySelector('form#register .errors')
+
+regForm.addEventListener('submit', validateRegisterForm);
+
+function  validateRegisterForm(e) {
+    e.preventDefault();
+
+   let firstname = document.querySelector('input#first-name');
+   let lastname = document.querySelector('input#last-name');
+    const email = document.querySelector('input#email');
+    const password = document.querySelector('input#pass');
+
+    let errors = [];
+
+    if (firstname.value == "") {
+        errors.push({text: "first name", el: firstname});
+    }
+    if (lastname.value == "") {
+        errors.push({text: "last name", el: lastname});
+    }
+    if (email.value == "") {
+        errors.push({text: "email", el: email});
+    }
+    if (password.value == "") {
+        errors.push({text: "password", el: password});
+    }
+
+    if (errors.length > 0) {
+       handle_errors(errors);
+        return false;
+    }
+
+    return true;
 }
+
+function handle_errors(errs) {
+    let str = "You have errors with the following fields: ";
+
+    errs.map((er) => {
+        er.el.classList.add('invalid');
+ });
+
+ errs[0].el.focus();
+
+ 
+}
+
