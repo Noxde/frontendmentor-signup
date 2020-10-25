4d511f55ae00e9741c890a42cf66667e47cc7c51
Noxde <70549780+Noxde@users.noreply.github.com>
2020-10-25 05:04:13 -0300

    almost finished the js

--- js/app.js
+++ js/app.js
@@ -7,15 +7,18 @@
 const regForm = document.querySelector('form#register')
 const errors_el = document.querySelector('form#register .errors')
 
+let firstname = document.querySelector('input#first-name');
+let lastname = document.querySelector('input#last-name');
+const email = document.querySelector('input#email');
+const password = document.querySelector('input#pass');
+
+let firstname1 = regForm.elements.namedItem("firstname");
+
 regForm.addEventListener('submit', validateRegisterForm);
 
 function  validateRegisterForm(e) {
     e.preventDefault();
 
-   let firstname = document.querySelector('input#first-name');
-   let lastname = document.querySelector('input#last-name');
-    const email = document.querySelector('input#email');
-    const password = document.querySelector('input#pass');
 
     let errors = [];
 
@@ -40,11 +43,16 @@ function  validateRegisterForm(e) {
     return true;
 }
 
+
+
 function handle_errors(errs) {
     let str = "You have errors with the following fields: ";
 
     errs.map((er) => {
         er.el.classList.add('invalid');
+        er.el.previousElementSibling.style.display = "block";
+        er.el.nextElementSibling.nextElementSibling.style.display = "block";
+        
  });
 
  errs[0].el.focus();
@@ -52,3 +60,72 @@ function handle_errors(errs) {
  
 }
 
+const email_reg = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+const pass_reg = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+
+firstname.addEventListener("input", validate);
+lastname.addEventListener("input", validate);
+password.addEventListener("input", validate);
+email.addEventListener("input", validate);
+
+
+function validate(e) {
+    let target = e.target;
+
+            if (target.name == "firstname") {
+                    if (target.value.length >= 4) {
+                    target.classList.add("valid");
+                    target.classList.remove("invalid");
+                    target.previousElementSibling.style.display = "none";
+                    target.nextElementSibling.nextElementSibling.style.display = "none";
+                } else {
+                    target.classList.remove("valid");
+                    target.classList.add("invalid");
+                    target.previousElementSibling.style.display = "block";
+                    target.nextElementSibling.nextElementSibling.style.display = "block";
+              
+                }
+            }
+    
+            if (target.name == "lastname") {
+                    if (target.value.length >= 4) {
+                    target.classList.add("valid");
+                    target.classList.remove("invalid");
+                    target.previousElementSibling.style.display = "none";
+                    target.nextElementSibling.nextElementSibling.style.display = "none";
+                } else {
+                    target.classList.remove("valid");
+                    target.classList.add("invalid");
+                    target.previousElementSibling.style.display = "block";
+                    target.nextElementSibling.nextElementSibling.style.display = "block";
+                }
+            }
+
+            if (target.name == "email") {
+                if (email_reg.test(target.value)) {
+                    target.classList.add("valid");
+                    target.classList.remove("invalid");
+                    target.previousElementSibling.style.display = "none";
+                    target.nextElementSibling.nextElementSibling.style.display = "none";
+                } else {
+                    target.classList.remove("valid");
+                    target.classList.add("invalid");
+                    target.previousElementSibling.style.display = "block";
+                    target.nextElementSibling.nextElementSibling.style.display = "block";
+                }
+            }
+
+            if (target.name == "password") {
+                if (pass_reg.test(target.value)) {
+                    target.classList.add("valid");
+                    target.classList.remove("invalid");
+                    target.previousElementSibling.style.display = "none";
+                    target.nextElementSibling.nextElementSibling.style.display = "none";
+                } else {
+                    target.classList.add("invalid");
+                    target.classList.remove("valid");
+                    target.previousElementSibling.style.display = "block";
+                    target.nextElementSibling.nextElementSibling.style.display = "block";
+                }
+            }
+}
